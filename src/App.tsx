e12c879081b336b8e24b328d8ddb9e790e2623b0
gbrogni <harden13brogni@gmail.com>
2024-03-20 19:39:39 -0300

    feat: Implementa theme toggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import './global.css';
 import { router } from './routes';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 export function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors />
+        <Toaster richColors />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }

56678fc088c8d8cca84c9ecf462a814fdd05e686
gbrogni <harden13brogni@gmail.com>
2024-03-20 12:19:17 -0300

    feat: Implementa form

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from 'react-router-dom';
 import './global.css';
 import { router } from './routes';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'sonner';
 
 export function App() {
 
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+
+      <Toaster richColors />
 
       <RouterProvider router={router} />
     </HelmetProvider>

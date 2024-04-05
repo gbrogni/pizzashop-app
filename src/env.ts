3191d69887fbd74920bf1af4b508334ed13bf395
gbrogni <harden13brogni@gmail.com>
2024-04-05 12:33:54 -0300

    feat: Adiciona loading state

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,9 @@ import { z } from 'zod'
 
 const envSchema = z.object({
     VITE_API_URL: z.string().url(),
+    VITE_ENABLE_API_DELAY: z.string().transform((val) => val === 'true'),
 })
+
 // @ts-ignore
 export const env = import.meta.env;
 

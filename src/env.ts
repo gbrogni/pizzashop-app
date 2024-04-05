7ac5fbcf788a92005cfd676f508077a183ec2ff3
gbrogni <harden13brogni@gmail.com>
2024-04-05 07:42:13 -0300

    fix

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,8 @@ import { z } from 'zod'
 const envSchema = z.object({
     VITE_API_URL: z.string().url(),
 })
+// @ts-ignore
+export const env = import.meta.env;
 
-export const env = envSchema.parse(import.meta.url)
+
+// export const env = envSchema.parse(import.meta.env)

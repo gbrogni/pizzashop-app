a9865e54a07ff0360c6ad13f11811c28043895aa
gbrogni <harden13brogni@gmail.com>
2024-04-08 19:54:47 -0300

    feat: Implementa detalhes do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button'
 import { TableCell, TableRow } from '@/components/ui/table'
 import { OrderDetails } from './order-details'
 import { Dialog, DialogTrigger } from '@radix-ui/react-dialog'
+import { useState } from 'react'
 
 interface OrderTableRowProps {
     order: {
@@ -20,10 +21,11 @@ interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+    const [isDetailsOpen, setIsDetailsOpen] = useState(false)
     return (
         <TableRow>
             <TableCell>
-                <Dialog>
+                <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
                     <DialogTrigger asChild>
                         <Button variant="outline" size="xs">
                             <Search className="h-3 w-3" />
@@ -31,7 +33,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                         </Button>
                     </DialogTrigger>
 
-                    <OrderDetails />
+                    <OrderDetails open={isDetailsOpen} orderId={order.orderId} />
                 </Dialog>
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
@@ -48,7 +50,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </TableCell>
             <TableCell className="font-medium">{order.customerName}</TableCell>
             <TableCell className="font-medium">
-                {order.total.toLocaleString('pt-BR', {
+                {(order.total / 100).toLocaleString('pt-BR', {
                     style: 'currency',
                     currency: 'BRL',
                 })}
